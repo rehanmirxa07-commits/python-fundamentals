10b8adea137ab7d885e8364a6c01532324a872fd
Rehan san <rehanmirxa07@gmail.com>
2026-09-16 19:52:12 +0530

    2ndoop

---
+++  01_basics_OOP_programs/02_OOP_program.py
@@ -0,0 +1,16 @@
+class WorkoutTracker:
+    def __init__(self,day):
+        self.day = day 
+        self.exercise = []
+    def add_exercise(self, name):
+        self.exercise.append(name)
+    def show_workout(self):
+        print(f"Wokouts for: {self.day}")
+        for exercise in self.exercise:
+            print(f" - {exercise}")
+
+my_workout = WorkoutTracker("Monday")
+my_workout.add_exercise("Bench Press")
+my_workout.add_exercise("Incline Dumbbell Press")
+
+my_workout.show_workout()
